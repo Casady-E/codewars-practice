ca56fc28d3efbe6571205876233328f3327ea7ea
Ethan Casady <ethancasady1@gmail.com>
2018-10-09 23:30:50 -0600

    Create even or odd function that checks if number is even or odd and not a string, returns corresponding output.

--- js/test.js
+++ js/test.js
@@ -8,10 +8,17 @@ and returns "Even" for even numbers or "Odd" for odd numbers.
 
 
 function even_or_odd(number) {
-  // ...
+if (typeof number === 'string') {
+    return "Error";
+  }else if(number % 2 === 0){
+    return "Even";
+  } else if (number % 2 != 0){
+    return "Odd";
+}
 }
 
 console.log(even_or_odd(2));
 console.log(even_or_odd(0));
 console.log(even_or_odd(7));
-console.log(even_or_odd(1)); 
+console.log(even_or_odd(1));
+console.log(even_or_odd("poop"));
