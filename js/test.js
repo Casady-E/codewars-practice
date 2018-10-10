0faf408c613425b0a307827aff474bb7d6fb7197
Ethan Casady <ethancasady1@gmail.com>
2018-10-09 23:00:41 -0600

    Setup Even or Odd challenge with directions and base code.

---
+++ js/test.js
@@ -0,0 +1,17 @@
+/*
+Even Or Odd
+8kyu
+
+Create a function (or write a script in Shell) that takes an integer as an argument
+and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+
+function even_or_odd(number) {
+  // ...
+}
+
+console.log(even_or_odd(2));
+console.log(even_or_odd(0));
+console.log(even_or_odd(7));
+console.log(even_or_odd(1)); 
