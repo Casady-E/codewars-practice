55c20f00370e0ba8cc61088086f3abc2ae1df495
Ethan Casady <ethancasady1@gmail.com>
2018-10-13 22:53:28 -0600

    Create switch expression foor operation to test what operator it is and execute it accordingly.

--- js/test.js
+++ js/test.js
@@ -13,7 +13,22 @@ The function should return result of numbers after applying the chosen operation
 
 function basicOp(operation, value1, value2)
 {
-
+  switch(operation){
+    case "*":
+    return value1 * value2;
+    break;
+    case "+":
+    return value1 + value2;
+    break;
+    case "-":
+    return value1 - value2;
+    break;
+    case "/":
+    return value1 / value2;
+    break;
+    default:
+    return "Oops";
+  }
 }
 
 console.log("Basic tests\n");
