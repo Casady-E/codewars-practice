ef6aa6047e39afc49c37a0eb78943290396c2634
Ethan Casady <ethancasady1@gmail.com>
2018-10-13 23:07:29 -0600

    Add directions for coding challenges including: Counting Sheep, Find the smallest integer in an array, Return Negative, Remove First and Last Character, String Repeat, Opposite number, and Sum of positive.

--- js/test.js
+++ js/test.js
@@ -1,38 +1,111 @@
 /*
-Basic Mathematical Operations
+Sum of positive
 8kyu
 
-Your task is to create a function that does four basic mathematical operations.
+You get an array of numbers, return the sum of all of the positives ones.
 
-The function should take three arguments - operation(string/char), value1(number), value2(number).
-The function should return result of numbers after applying the chosen operation.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+*/
+
+
+/*
+Opposite number
+8kyu
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?
 
 
 */
 
 
-function basicOp(operation, value1, value2)
-{
-  switch(operation){
-    case "*":
-    return value1 * value2;
-    break;
-    case "+":
-    return value1 + value2;
-    break;
-    case "-":
-    return value1 - value2;
-    break;
-    case "/":
-    return value1 / value2;
-    break;
-    default:
-    return "Oops";
-  }
-}
 
-console.log("Basic tests\n");
-console.log(basicOp('+', 4, 7)); //11
-console.log(basicOp('-', 15, 18)); //-3
-console.log(basicOp('*', 5, 5)); //25
-console.log(basicOp('/', 49, 7)); //7
+
+
+/*
+String Rpeat
+8kyu
+
+Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
+
+
+
+/*
+Remove First and Last Character
+8kyu
+
+It's pretty straightforward. Your goal is to create a function that removes the
+first and last characters of a string. You're given one parameter, the original string.
+You don't have to worry with strings with less than two characters.
+
+*/
+
+
+/*
+Return Negative
+8kyu
+
+In this simple assignment you are given a number and have to make it negative.
+But maybe the number is already negative?
+
+Example:
+
+makeNegative(1); // return -1
+makeNegative(-5); // return -5
+makeNegative(0); // return 0
+makeNegative(0.12); // return -0.12
+Notes:
+
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+*/
+
+/*
+Find the smallest integer in and array
+8kyu
+
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+*/
+
+
+/*
+Counting Sheep
+8kyu
+
+Consider an array of sheep where some sheep may be missing from their place. We need
+a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+*/
